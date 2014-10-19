7380ad93d86c02b40f32559ebbd0608a103c8d93
hai9999 <thai.viethai@zecchino.vn>
2014-10-20 01:27:04 +0700

    variable demo

--- src/vari/test.go
+++ src/vari/test.go
@@ -2,8 +2,8 @@ package main
 import "fmt"
 func main() {
 //assign variable
-x1 := 5
-fmt.Println("x is",x1)
+/*x1 := 5
+fmt.Println("x is",x1)*/
 
 // Multiple Variables
 /*var (
@@ -17,5 +17,22 @@ fmt.Println("My dog's name is",a,b,c)*/
 /*const x string = "Hello World"
 fmt.Println(x)*/
 
+//String
+//Replace string
+/*s := "hello"
+c := []byte(s)  // convert string to []byte type
+c[0] = 'c'
+s2 := string(c)  // convert back to string type
+fmt.Printf("%s\n", s2)*/
+
+//Cut string
+/*s := "hello"
+s = string(s[1:]) // you cannot change string values by index, but you can get values instead.
+fmt.Printf("%s\n", s)*/
+
+//Mupltiline string
+/*m := `hello
+world`
+fmt.Printf("%s\n", m)*/
 
 }
