a1ff6c4b1fbabc144f360c2d4e9710dbb55f4935
hai9999 <thai.viethai@zecchino.vn>
2014-10-20 01:44:42 +0700

    demo input

--- src/demo/input.go
+++ src/demo/input.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Enter a number: ")
-	var input float64
+	/*fmt.Print("Enter a number: ")
+	var input float32
 	fmt.Scanf("%f", &input)
 	output := input * 2
-	fmt.Println(output)
+	fmt.Printf("%.2f",output)*/
+
+	i := 12
+	if i > 10 {
+	 fmt.Println("Big")
+	} else {
+	 fmt.Println("Small")
+	}
+
 }
