a4edd8f63b8ab2266e29ff2478c0c5bc511a1505
hai9999 <thai.viethai@zecchino.vn>
2014-10-20 01:16:24 +0700

    variable demo

---
+++ src/hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"newmath"
+)
+
+func main() {
+	fmt.Printf("Hello, world. 2  Sqrt(2) = %v\n", newmath.Sqrt(2))
+}
---
+++ src/vari/test.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+func main() {
+//assign variable
+x1 := 5
+fmt.Println("x is",x1)
+
+// Multiple Variables
+/*var (
+ a = 5
+ b = 10
+ c = 15
+)
+fmt.Println("My dog's name is",a,b,c)*/
+
+//Constants
+/*const x string = "Hello World"
+fmt.Println(x)*/
+
+
+}
