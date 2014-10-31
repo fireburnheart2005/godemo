f5b8509a7bdfa64b8f237d4a0c6cb6b110c24fd6
hai9999 <thai.viethai@zecchino.vn>
2014-11-01 03:17:18 +0700

    upgrade variable demo

--- src/vari/test.go
+++ src/vari/test.go
@@ -1,41 +1,56 @@
 package main
-import "fmt"
-func main() {
-//Assign variable
-/*x1 := 5
-fmt.Println("x is",x1)
-*/
 
-// Multiple Variables
-/*var (
- a = 5
- b = 10
- c = 15
+import (
+	"fmt"
 )
-fmt.Println("My dog's name is",a,b,c)*/
 
-//Constants
-/*const x string = "Hello World"
-fmt.Println(x)
-*/
+func main() {
+	/*---------------Assign variable------------*/
+	//Way 1
+	/*var x string
+	x = "xin chào"
+	fmt.Println(x)*/
+	//Way 2
+	/*var x1 string = "Hello World"
+	fmt.Println(x1)*/
 
-//String
-//Replace string
-/*s := "hello"
-c := []byte(s)  // convert string to []byte type
-c[0] = 'c'
-s2 := string(c)  // convert back to string type
-fmt.Printf("%s\n", s2)*/
+	/*x1 := 5
+	fmt.Println("x is", x1)*/
 
-//Cut string
-/*s := "hello"
-s = string(s[1:]) // you cannot change string values by index, but you can get values instead.
-fmt.Printf("%s\n", s)*/
+	// Multiple Variables
+	/*var (
+	   a = 5
+	   b = 10
+	   c = 15
+	  )
+	  fmt.Println("My dog's name is",a,b,c)*/
 
-//Mupltiline string
-/*m := `hello
-world`
-fmt.Printf("%s\n", m)
-*/
+	//Constants
+	/*const x string = "Hello World"
+	fmt.Println(x)*/
+
+	//String
+	//Replace string
+	/*s := "hello"
+	  c := []byte(s)  // convert string to []byte type
+	  c[0] = 'c'
+	  s2 := string(c)  // convert back to string type
+	  fmt.Printf("%s\n", s2)*/
+
+	//Cut string
+	/*s := "hello"
+	  s = string(s[1:]) // you cannot change string values by index, but you can get values instead.
+	  fmt.Printf("%s\n", s)*/
+
+	//Mupltiline string
+	/*m := `hello
+	  world`
+	  fmt.Printf("%s\n", m)
+	*/
+
+	/*-------- compare string ------------*/
+	/*var x string = "hello"
+	var y string = "hello"
+	fmt.Println(x == y)*/
 
 }
